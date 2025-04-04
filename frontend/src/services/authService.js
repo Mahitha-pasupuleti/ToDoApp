833b46ecf8419fbb355d918510bc288c4d12ecf4
Mahitha-pasupuleti <mahithapasupuleti23@gmail.com>
2025-04-03 18:21:05 -0700

    Third commit

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,13 +1,13 @@
 import { getRequest, postRequest } from "./api.js";
 
 export const signUp = async (regsitrationData) => {
-    return await postRequest("/register", regsitrationData);
+    return await postRequest("/api/v1/users/register", regsitrationData);
 }
 
 export const login = async (loginData) => {
-    return await postRequest("/login", loginData);
+    return await postRequest("/api/v1/users/login", loginData);
 }
 
 export const logout = async (accessToken) => {
-    return await postRequest("/logout", {}, accessToken);
+    return await postRequest("/api/v1/users/logout", {}, accessToken);
 }
--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,17 +1,17 @@
 import { getRequest, postRequest, deleteRequest, putRequest } from "./api.js";
 
 export const getAllTasks = async (accessToken) => {
-    return await getRequest("/getTasks", accessToken);
+    return await getRequest("/api/v1/users/getTasks", accessToken);
 }
 
 export const addNewTask = async (newEntry, accessToken) => {
-    return await postRequest("/addTask", newEntry, accessToken);
+    return await postRequest("/api/v1/users/addTask", newEntry, accessToken);
 }
 
 export const deleteATask = async (accessToken, taskId) => {
-    return await deleteRequest("/deleteATask", {}, accessToken, taskId);
+    return await deleteRequest("/api/v1/users/deleteATask", {}, accessToken, taskId);
 }
 
 export const updateATask = async (updateEntry, accessToken, taskId) => {
-    return await putRequest("/updateATask", updateEntry, accessToken, taskId);
+    return await putRequest("/api/v1/users/updateATask", updateEntry, accessToken, taskId);
 }
